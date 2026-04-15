8c71f4ce90a422ea17f2e05df0cd1907279bee5c
Denis Lazarev <mahrizh@gmail.com>
2026-04-15 15:33:41 +0300

    Sliced list draft

---
+++ Runtime/SlicedList.cs
@@ -0,0 +1,40 @@
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace Clipper
+{
+	public struct SlicedList<T> : INativeDisposable
+		where T : unmanaged
+	{
+		private NativeList<T> list;
+		private NativeList<int> slices;
+
+		public SlicedList(Allocator allocator)
+		{
+			list = new NativeList<T>(allocator);
+			slices = new NativeList<int>(allocator);
+		}
+
+		public void Dispose()
+		{
+			list.Dispose();
+			slices.Dispose();
+		}
+
+		public JobHandle Dispose(JobHandle inputDeps)
+			=> JobHandle.CombineDependencies(list.Dispose(inputDeps), slices.Dispose(inputDeps));
+
+		public void AddItem(T item)
+			=> list.Add(item);
+
+		public void AddSlice()
+			=> slices.Add(list.Length);
+
+		public NativeSlice<T> GetSlice(int index)
+		{
+			int sliceBegin = 0 <= index ? 0 : slices[index - 1];
+			int sliceEnd = index < slices.Length ? slices[index] : list.Length;
+			return list.AsArray().Slice(sliceBegin, sliceEnd - sliceBegin);
+		}
+	}
+}
